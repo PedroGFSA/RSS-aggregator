f0361c638c8b1d43cc487ac93b1f724e2751e6d6
PedroGFSA <pedrogfsa2002@gmail.com>
2024-03-30 00:23:45 -0300

    Boilerplate complete

--- main.go
+++ main.go
@@ -29,6 +29,13 @@ func main() {
 		AllowCredentials: false,
 		MaxAge:           300,
 	}))
+
+	v1Router := chi.NewRouter()
+
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/err", handleErr)
+	router.Mount("/v1", v1Router)
+
 	server := &http.Server{
 		Handler: router,
 		Addr:    ":" + portString,
